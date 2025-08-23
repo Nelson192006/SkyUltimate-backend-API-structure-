fd16fa5bf1fd24511f274350e920004e1a0309d8
Nelson192006 <nelsongodspower24@gmail.com>
2025-08-23 16:08:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
-const userRoutes = require("./userRoutes");     // ✅ No folder
-const orderRoutes = require("./orderRoutes");   // ✅ No folder
+const userRoutes = require("./userRoutes");
+const orderRoutes = require("./orderRoutes");
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 // Routes
