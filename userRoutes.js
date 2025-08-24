e9e31ccad78a038f8cd47d6b2a94aa7307e1f7b2
Nelson192006 <nelsongodspower24@gmail.com>
2025-08-24 01:41:26 +0100

    Update userRoutes.js

--- userRoutes.js
+++ userRoutes.js
@@ -1,16 +1,16 @@
 const express = require("express");
-const { registerUser, loginUser, getProfile } = require("./userController"); 
+const { registerUser, loginUser, getProfile } = require("./userController");
 const { authMiddleware } = require("./authMiddleware");
 
 const router = express.Router();
 
-// Register a new user
+// Register
 router.post("/register", registerUser);
 
-// Login a user
+// Login
 router.post("/login", loginUser);
 
-// Get user profile (protected route)
+// Profile (protected)
 router.get("/profile", authMiddleware, getProfile);
 
 module.exports = router;
