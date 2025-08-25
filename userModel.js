d0cc4a68e1e5bde978eaca7b2ccd9a7386ed8201
Nelson192006 <nelsongodspower24@gmail.com>
2025-08-25 22:15:17 +0100

    Update userModel.js

--- userModel.js
+++ userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
   {
@@ -13,5 +14,19 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+// Encrypt password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Match entered password to hashed password in DB
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 module.exports = mongoose.model("User", userSchema);
