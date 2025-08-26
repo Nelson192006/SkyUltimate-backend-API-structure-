fc2fc1eed4663660a8d748901c3d0fe697defa8c
Nelson192006 <nelsongodspower24@gmail.com>
2025-08-26 01:09:52 +0100

    Create roleMiddleware.js

---
+++ roleMiddleware.js
@@ -0,0 +1,11 @@
+// roleMiddleware.js
+const allowRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!req.user?.role || !roles.includes(req.user.role)) {
+      return res.status(403).json({ message: "Forbidden for your role" });
+    }
+    next();
+  };
+};
+
+module.exports = { allowRoles };
