5ffed1d8e3c9c81cbd790f19d779aa2b11c35109
Nelson192006 <nelsongodspower24@gmail.com>
2025-08-26 11:26:45 +0100

    Delete Announcement.js

--- Announcement.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const announcementSchema = new mongoose.Schema({
-    message: { type: String, required: true },
-    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Announcement', announcementSchema);
