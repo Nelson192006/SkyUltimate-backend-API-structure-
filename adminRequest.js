b6e0556e83c83ba6d2ac4dc0877113f59eafb965
Nelson192006 <nelsongodspower24@gmail.com>
2025-08-26 12:48:46 +0100

    Update adminRequest.js

--- adminRequest.js
+++ adminRequest.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const adminRequestModel = require('./adminRequest'); // lowercase
-const authMiddleware = require('./middleware/auth');
+const adminRequestModel = require('./adminRequest');
+const authMiddleware = require('./auth');
 
-// Create admin request
 router.post('/', authMiddleware, async (req, res) => {
     const { type, reason, requestedBy } = req.body;
     const request = await adminRequestModel.create({ type, reason, requestedBy });
     res.json(request);
 });
 
-// Get all admin requests
 router.get('/', authMiddleware, async (req, res) => {
     const requests = await adminRequestModel.find().populate('requestedBy', 'fullName role');
     res.json(requests);
