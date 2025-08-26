704460448a77febb01fcb35a0b2dcbc49925fbd5
Nelson192006 <nelsongodspower24@gmail.com>
2025-08-26 12:49:30 +0100

    Update Settings.js

--- Settings.js
+++ Settings.js
@@ -1,22 +1,19 @@
 const mongoose = require("mongoose");
 
-const settingsSchema = new mongoose.Schema(
-  {
+const settingsSchema = new mongoose.Schema({
     adminRegistrationEnabled: { type: Boolean, default: false },
     commissionRateAgent: { type: Number, default: 0.7 },
     bankDetails: {
-      bankName: { type: String, default: "Your Bank" },
-      accountNumber: { type: String, default: "0000000000" },
-      accountHolderName: { type: String, default: "SkyUltimate" },
+        bankName: { type: String, default: "Your Bank" },
+        accountNumber: { type: String, default: "0000000000" },
+        accountHolderName: { type: String, default: "SkyUltimate" },
     },
-  },
-  { timestamps: true }
-);
+}, { timestamps: true });
 
 settingsSchema.statics.get = async function () {
-  let doc = await this.findOne();
-  if (!doc) doc = await this.create({});
-  return doc;
+    let doc = await this.findOne();
+    if (!doc) doc = await this.create({});
+    return doc;
 };
 
 module.exports = mongoose.model("Settings", settingsSchema);
