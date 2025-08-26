09792b4f0cb4281e56fb52ad785061d50b0cd24c
Nelson192006 <nelsongodspower24@gmail.com>
2025-08-26 01:08:20 +0100

    Update User.js

--- User.js
+++ User.js
@@ -4,45 +4,44 @@ const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
   {
-    name: {
+    name: { type: String, required: true, trim: true },
+    email: { type: String, required: true, unique: true, lowercase: true, trim: true },
+    password: { type: String, required: true, minlength: 6 },
+    role: {
       type: String,
-      required: [true, "Please add a name"],
+      enum: ["Customer", "Agent", "Admin", "SuperAdmin"],
+      default: "Customer",
     },
-    email: {
-      type: String,
-      required: [true, "Please add an email"],
-      unique: true,
-      match: [
-        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-        "Please enter a valid email",
-      ],
+    phone: { type: String, trim: true },
+    bankDetails: {
+      bankName: String,
+      accountNumber: String,
+      accountHolderName: String,
     },
-    password: {
-      type: String,
-      required: [true, "Please add a password"],
-      minlength: 6,
-    },
+    isSuspended: { type: Boolean, default: false },
+    isAvailable: { type: Boolean, default: false }, // for agents
+    ratingAverage: { type: Number, default: 0 },
+    ratingCount: { type: Number, default: 0 },
+    pendingPayout: { type: Number, default: 0 },
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
 
-// 🔑 Encrypt password before saving
+// hash password before save
 userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) {
-    return next();
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
   }
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
-// 🔑 Compare entered password to hashed password in DB
-userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
+// compare password
+userSchema.methods.matchPassword = function (entered) {
+  return bcrypt.compare(entered, this.password);
 };
 
-const User = mongoose.model("User", userSchema);
-
-module.exports = User;
+module.exports = mongoose.model("User", userSchema);
