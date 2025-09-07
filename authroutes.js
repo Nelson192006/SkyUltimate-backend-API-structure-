19e4e53e5baf3bab0f8acad83a10bb14c8800719
Nelson192006 <nelsongodspower24@gmail.com>
2025-09-07 05:47:04 +0100

    Update authroutes.js

--- authroutes.js
+++ authroutes.js
@@ -77,13 +77,11 @@ router.post("/register", async (req, res) => {
     if ((role === "Agent" || role === "Admin") && !bankDetails) {
       return res.status(400).json({ message: "Bank details required for Agents and Admins" });
     }
-
-    const hashed = await bcrypt.hash(password, 10);
     const user = await User.create({
       name,
       email,
-      password: hashed,
-      role,
+      password,      //raw password goes here
+     role,
       phone,
       bankDetails,
     });
